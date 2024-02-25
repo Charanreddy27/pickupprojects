ca6aa218e1e3e76175db1c4402a71247baf4009a
Charan <charanbapireddy@gmail.com>
2024-02-25 21:17:29 +0530

    first-project date

--- src/config/data.js
+++ src/config/data.js
@@ -9,7 +9,7 @@ export const blogList = [
       "This Invoice Management System project  focuses mainly on keeping track of invoices as well as their product sales. Also, the system displays all the products with their respective details. In addition, the system allows managing customers’ records. Evidently, this project only contains an admin panel. In an overview of this web application, an administrator has full control over the system. He/she can manage invoices, users, customers, and products. Here, each and every section has its own respective details such as name and other important details. However, the admin set random product names with rates for generating invoices. Or the admin can select from an existing record.Likewise, the system also allows managing these records. For this, the user has to provide product names with the rate amount. Also, this particular section is a kind of optional field for managing invoices. Meanwhile, the completion of all these fields leads us to customer management. In fact, this section requires a number of details for its management. It includes the name of the customer with its contact, addresses information. Here, this field can be optional for maintaining invoices too. The user can select numerous product items under the same invoices. With it, the system automatically displays the rate amount of each for the billing process.",
     authorName: 'pickupprojects',
     authorAvatar: '/assets/images/author.jpg',
-    createdAt: 'June 03, 2021',
+    createdAt: 'Feb 25, 2024',
     cover: project1,
   },
 //   {
